b1b2a3c39973f81b65223614b850cd68bb3b0dd3
rani23986 <rani.bisen23@mmit.edu.in>
2025-10-16 11:05:08 +0530

    subject decision making

---
+++ Artificial_Intelligence_Lab/experiment.py
@@ -0,0 +1,17 @@
+subject1=(input("Enter the subjects"))
+subject2=(input("Enter second subject"))
+if(subject1=="maths"   and   subject2=="physics"):
+     	print("Branch: MECHANICAL ENGINEERING")
+       	 
+elif(subject1=="programming"  and  subject2=="maths"):
+     	print("Branch:COMPUTER ENGINEERING")
+        	 
+elif(subject1=="circuits "  and  subject2=="maths"):
+     	print("Branch:ELECTRONICS ENGINEERING")
+       	 
+elif(subject1=="biology"  and  subject2=="chemistry"):
+    	print("Branch:BIOTECHNOLOGY")
+      	 
+else:
+    	print("Invalid")
+
